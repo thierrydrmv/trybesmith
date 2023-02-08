b2196130f9db69c6c834510f83d2d2935f9446b7
Thierry <varelathierry@gmail.com>
2023-02-08 15:52:02 -0300

    users endpoint validation

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
 import UserController from '../controllers/usersController';
+import { 
+  usersLevel, 
+  usersPassword, 
+  usersUsername, 
+  usersVocation } from '../middleware/usersMiddleware';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/users', userController.newUser);
+router.post(
+  '/users',
+  usersUsername,
+  usersVocation,
+  usersLevel,
+  usersPassword,
+  userController.newUser,
+);
 
 export default router;
